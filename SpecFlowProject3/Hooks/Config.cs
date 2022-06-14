b610226118cb2e3629e53334b1d320ee0ec3e49d
mohamedkammoun123 <85997655+mohamedkammoun123@users.noreply.github.com>
2022-06-14 09:29:15 +0100

    Add files via upload

---
+++ SpecFlowProject3/Hooks/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowProject3.Hooks
+{
+    class Config
+    {
+       public String report_html_path;
+       public  String chrome_driver_path;
+       public  int mongodb_port;
+       public  String mongodb_ip;
+       public String klov_server_ip;
+       public int klov_server_port;
+       public string svm_model_ip;
+    }
+}
